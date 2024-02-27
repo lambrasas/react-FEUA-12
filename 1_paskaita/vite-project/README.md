724056e698def3001c4ed258fce53b6f68ec2646
= <=>
2024-02-27 19:59:40 +0200

    First Lesson

---
+++ 1_paskaita/vite-project/README.md
@@ -0,0 +1,5 @@
+React FEUA-12
+
+komponento pavadinimas turi atitikti failo pavadinima
+komponentas visad prasideda didziaja raide
+JSX(JavaScript XML) - HTMl in React (Javascript +HTML)
---
+++ 1_paskaita/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+  const name = "Linas"
+  const question = "How are you?"
+  return (
+  <div>
+    <h1>Hello {name}</h1>
+    <h2>{question}</h2>
+  </div>
+  )
+
+}
+export default App;
